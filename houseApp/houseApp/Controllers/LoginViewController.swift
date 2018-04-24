e2e866c7bf6f8c315d5d21432dbbfe63add0f450
Haydee Rodriguez <hrodriguez@nearsoft.com>
2018-04-24 15:46:52 -0500

    Add conection with firebase and googleinfoplist

--- houseApp/houseApp/Controllers/LoginViewController.swift
+++ houseApp/houseApp/Controllers/LoginViewController.swift
@@ -20,6 +20,9 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func loginFBButton(_ sender: UIButton) {
+        
+    }
+    
 }
 
