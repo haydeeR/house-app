3035b10d0e7836a28fed1c217ba39a25ba9f822b
Haydee Rodriguez <hrodriguez@nearsoft.com>
2018-04-25 12:54:19 -0500

    Add Object mapper and promise kit for the request of database and now we can handler data of database in firebase

--- houseApp/houseApp/DataHelpers/Database/DBModel.swift
+++ houseApp/houseApp/DataHelpers/Database/DBModel.swift
@@ -7,3 +7,7 @@
 //
 
 import Foundation
+protocol DBModel {
+    var id: String { get set }
+    func toDictionary() -> [String: Any]
+}
--- houseApp/houseApp/DataHelpers/Database/FirebasePath.swift
+++ houseApp/houseApp/DataHelpers/Database/FirebasePath.swift
@@ -11,4 +11,6 @@ import Foundation
 enum FirebasePath: String {
     case users
     case houses
+    case favoriteHouses
+    case pictures
 }
