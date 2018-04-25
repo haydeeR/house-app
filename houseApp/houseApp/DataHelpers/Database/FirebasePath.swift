5b0acfaf7ddad8ed3112908c0850b0af0001d1a8
Haydee Rodriguez <hrodriguez@nearsoft.com>
2018-04-25 00:19:25 -0500

    Create Datahelpers, data model, databasehandler and database path

---
+++ houseApp/houseApp/DataHelpers/Database/FirebasePath.swift
@@ -0,0 +1,14 @@
+//
+//  FirebasePath.swift
+//  houseApp
+//
+//  Created by Haydee Rodriguez on 4/24/18.
+//  Copyright © 2018 Haydee Rodriguez. All rights reserved.
+//
+
+import Foundation
+
+enum FirebasePath: String {
+    case users
+    case houses
+}
